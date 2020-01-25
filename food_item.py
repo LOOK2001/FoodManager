20dd9329f9ca277fb26365a325496b2714aa7067
xicheng Wang <shengwang1996@126.com>
2020-01-25 13:23:48 -0500

    update food_item

--- food_item.py
+++ food_item.py
@@ -3,7 +3,9 @@ from flask import Flask, escape, requests
 class FoodItem:
     name = 'default'
     cal = 30
+    price = 0
 
-    def __init__(self, food_name,calories):
+    def __init__(self, food_name,calories, price):
         self.name = food_name
         self.cal = calories
+        self.price = price
--- recipe.py
+++ recipe.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import food_item
 
 class Recipe:
     ingredients = []
