31985685cf0cabffd266bdf54cb2072ad8d80f16
xicheng Wang <shengwang1996@126.com>
2020-01-25 14:27:58 -0500

    update food_item and recipe

--- food_item.py
+++ food_item.py
@@ -4,8 +4,22 @@ class FoodItem:
     name = 'default'
     cal = 30
     price = 0
+    quantity = 0
 
-    def __init__(self, food_name,calories, price):
+    def __init__(self, food_name,calories, price, quantity):
         self.name = food_name
         self.cal = calories
         self.price = price
+        self.quantity = quantity
+
+    def setFoodName(self, name):
+        self.name = name
+
+    def setCalories(self, calories):
+        self.calories = calories
+
+    def setPrice(self, price):
+        self.price = price
+
+    def setQuantity(self, quantity):
+        self.quantity = quantity
--- recipe.py
+++ recipe.py
@@ -1,16 +1,57 @@
 from flask import Flask
-import food_item
+from food_item import food_item 
 
 class Recipe:
-    ingredients = []
+    ingredients = {}
     meal_name = "meal"
+    total_price = 0
 
     def __init__(self,ingredients,name):
         self.ingredients = ingredients
         self.meal_name = name
 
+    def addItem(self, item, quantity):
+        if item in self.ingredients.keys():
+            self.ingredients[item] = self.ingredients[item] + quantity
+        else:
+            self.ingredients[item] = quantity
+
+    def deleteByItem(self, item):
+        pass
+
+    def deleteByIndex(self, index):
+        del self.ingredients[index]
+
+    def getTotalCal(self):
+        cal = 0
+        for item in self.ingredients.keys():
+            cal += (item.cal * self.ingredients[item])
+
+        return cal
+
+    def getTotalPrice(self):
+        price = 0
+        for item in self.ingredients.keys():
+            price += (item.price * self.ingredients[item])
+
+        return price
+
+
+
 class RecipeList:
-    recipies = []
+    __recipies = []
 
     def __init__(self,recipies):
-        self.recipies = recipies
+        self.__recipies = recipies
+
+    def addRecipe(self, recipe):
+        self.__recipies.append(recipe)
+
+    def deleteRecipeByIndex(self, index):
+        del self.__recipies[index]
+
+    def deleteRecipeByItem(self, item):
+        self.__recipies.remove(item)
+
+    def getRecipies(self):
+        return self.__recipies
