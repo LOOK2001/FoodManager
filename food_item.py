5b8837c19c14f2d06e52cbac08a7bb04ab7ffd12
joshlin5 <47061522+joshlin5@users.noreply.github.com>
2020-01-25 20:17:40 -0500

    added food category to food_item.py

--- food_item.py
+++ food_item.py
@@ -1,14 +1,24 @@
+class FoodType(Enum):
+    Vegetables = 1
+    Fruits = 2
+    Grains_Beans_Nuts = 3
+    Meat_Poultry = 4
+    Fish_Seafood = 5
+    Dairy_Foods = 6
+
 class FoodItem:
     name = 'default'
     cal = 0
     price = 0
     quantity = 0
+    foodCategory = FoodType.Vegetables
 
-    def __init__(self, food_name,calories, price, quantity):
+    def __init__(self, food_name,calories, price, quantity, foodType):
         self.name = food_name
         self.cal = calories
         self.price = price
         self.quantity = quantity
+        self.foodCategory = foodType
 
     def setFoodName(self, name):
         self.name = name
@@ -33,3 +43,6 @@ class FoodItem:
 
     def getName(self):
         return self.name
+
+    def getFoodType(self):
+        return self.foodCategory
