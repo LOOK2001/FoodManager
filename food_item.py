2e56960baa950e250c00ade12079e8dee485c7b7
joshlin5 <47061522+joshlin5@users.noreply.github.com>
2020-01-26 00:06:21 -0500

    cheng

--- food_item.py
+++ food_item.py
@@ -1,7 +1,48 @@
+class FoodType(Enum):
+    Vegetables = 1
+    Fruits = 2
+    Grains_Beans_Nuts = 3
+    Meat_Poultry = 4
+    Fish_Seafood = 5
+    Dairy_Foods = 6
+
 class FoodItem:
     name = 'default'
+    cal = 0
+    price = 0
     quantity = 0
+    foodCategory = FoodType.Vegetables
 
-    def __init__(self, food_name,quan):
+    def __init__(self, food_name,calories, price, quantity, foodType):
         self.name = food_name
-        self.quantity = quan
+        self.cal = calories
+        self.price = price
+        self.quantity = quantity
+        self.foodCategory = foodType
+
+    def setFoodName(self, name):
+        self.name = name
+
+    def setCalories(self, calories):
+        self.calories = calories
+
+    def setPrice(self, price):
+        self.price = price
+
+    def setQuantity(self, quantity):
+        self.quantity = quantity
+    
+    def getQuantity(self):
+        return self.quantity
+
+    def getCal(self):
+        return self.cal
+
+    def getPrice(self):
+        return self.price
+
+    def getName(self):
+        return self.name
+
+    def getFoodType(self):
+        return self.foodCategory
