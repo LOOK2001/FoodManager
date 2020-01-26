3bbcf19db13bf15198b3db7795dc598c2882e715
ejblom5 <31282336+ejblom5@users.noreply.github.com>
2020-01-25 20:39:07 -0500

    Add files via upload

--- food_item.py
+++ food_item.py
@@ -1,48 +1,7 @@
-class FoodType(Enum):
-    Vegetables = 1
-    Fruits = 2
-    Grains_Beans_Nuts = 3
-    Meat_Poultry = 4
-    Fish_Seafood = 5
-    Dairy_Foods = 6
-
 class FoodItem:
     name = 'default'
-    cal = 0
-    price = 0
     quantity = 0
-    foodCategory = FoodType.Vegetables
 
-    def __init__(self, food_name,calories, price, quantity, foodType):
+    def __init__(self, food_name,quan):
         self.name = food_name
-        self.cal = calories
-        self.price = price
-        self.quantity = quantity
-        self.foodCategory = foodType
-
-    def setFoodName(self, name):
-        self.name = name
-
-    def setCalories(self, calories):
-        self.calories = calories
-
-    def setPrice(self, price):
-        self.price = price
-
-    def setQuantity(self, quantity):
-        self.quantity = quantity
-    
-    def getQuantity(self):
-        return self.quantity
-
-    def getCal(self):
-        return self.cal
-
-    def getPrice(self):
-        return self.price
-
-    def getName(self):
-        return self.name
-
-    def getFoodType(self):
-        return self.foodCategory
+        self.quantity = quan
